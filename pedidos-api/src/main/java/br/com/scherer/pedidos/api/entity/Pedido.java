8e90a0cc9eea3a701a2fbfd96e075e15265831fb
Cleyton Luiz Scherer <cleytonscherer@gmail.com>
2025-10-02 21:41:32 -0300

    commit inicial do projeto

---
+++ pedidos-api/src/main/java/br/com/scherer/pedidos/api/entity/Pedido.java
@@ -0,0 +1,35 @@
+package br.com.scherer.pedidos.api.entity;
+
+import br.com.scherer.pedidos.api.enums.StatusPedido;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    private UUID    id = UUID.randomUUID();
+
+    private String  cliente;
+
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    private Double  valorTotal;
+
+    private String  emailNotificacao;
+
+    private StatusPedido    statusPedido = StatusPedido.EM_PROCESSAMENTO;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime   dataHora = LocalDateTime.now();
+
+
+}
---
+++ pedidos-api/src/main/java/br/com/scherer/pedidos/api/enums/StatusPedido.java
@@ -0,0 +1,7 @@
+package br.com.scherer.pedidos.api.enums;
+
+public enum StatusPedido {
+
+    EM_PROCESSAMENTO,
+    PROCESSADO
+}
